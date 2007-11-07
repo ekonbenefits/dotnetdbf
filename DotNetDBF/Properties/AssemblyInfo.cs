5a2cdfcf36d0c1e19b8fc0745ba27ba59b72a67e
jtuley@gmail.com <jtuley@gmail.com>
2007-11-07 22:03:43 +0000

    Added missing AssemblyInfo.cs
    --HG--
    extra : convert_revision : svn%3A3f93beca-5d3e-0410-89fa-1d2fe7116a16/trunk%404

---
+++ DotNetDBF/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly : AssemblyTitle("DotNetDBF")]
+[assembly : AssemblyDescription("")]
+[assembly : AssemblyConfiguration("")]
+[assembly : AssemblyCompany("")]
+[assembly : AssemblyProduct("DotNetDBF")]
+[assembly : AssemblyCopyright("Copyright ©  2007")]
+[assembly : AssemblyTrademark("")]
+[assembly : AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly : ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly : Guid("950dfc0d-63ac-43fc-8e58-77a7d9cca63a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+
+[assembly : AssemblyVersion("1.0.0.0")]
+[assembly : AssemblyFileVersion("1.0.0.0")]
