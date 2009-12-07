f51f21796ccd48523d1314a24965c1ac74fd8724
jayt <none@none>
2009-12-07 11:55:48 -0600

    version number

--- DotNetDBF/Properties/AssemblyInfo.cs
+++ DotNetDBF/Properties/AssemblyInfo.cs
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly : AssemblyVersion("2.0.0.0")]
-[assembly : AssemblyFileVersion("1.0.0.0")]
+[assembly : AssemblyVersion("2.0.0.1")]
