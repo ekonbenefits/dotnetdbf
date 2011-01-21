b84d745e6a18512fe77e5cac4d3f1aba768b4ee2
jayt <none@none>
2011-01-21 09:12:19 -0600

    Added debugging property for dynamic proxy. Refactored methods to consolidate with optional paramters. added documenentation. started nuget structure.

--- DotNetDBF.Enumerable/Enumerable.cs
+++ DotNetDBF.Enumerable/Enumerable.cs
@@ -11,58 +11,47 @@ namespace DotNetDBF.Enumerable
     {
 
 
-        static public IEnumerable<T> AllRecords<T>(this DBFReader reader, T prototype) where T : class 
+
+        /// <summary>
+        /// Return all the records. T should be interface with getter properties that match types and names of the database. 
+        /// Optionally instead of T being and interface you can pass in an annoymous object with properties that match that 
+        /// database and then you'll get an IEnumerable of that annonymous type with the data filled in.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="reader">The reader.</param>
+        /// <param name="prototype">The prototype. Anonymous class instance</param>
+        /// <returns></returns>
+        static public IEnumerable<T> AllRecords<T>(this DBFReader reader, T prototype =null) where T : class 
         {
             var tType = typeof(T);
+            
+            var tProperties = tType.GetProperties();
+            var tProps = tProperties
+                .Select(
+                it =>
+                Array.FindIndex(reader.Fields,
+                                jt => jt.Name.Equals(it.Name, StringComparison.InvariantCultureIgnoreCase))).ToArray();
+            
+            var tOrderedProps = tProps.OrderBy(it => it).ToArray();
+            var tReturn = new List<T>();
+
+
             if (tType.GetCustomAttributes(typeof(CompilerGeneratedAttribute), false).Any())
             {
-                var tProps = tType.GetProperties()
-                    .Select(
-                    it =>
-                    Array.FindIndex(reader.Fields,
-                                    jt => jt.Name.Equals(it.Name, StringComparison.InvariantCultureIgnoreCase))).ToArray();
-
-                var tOrderedProps = tProps.OrderBy(it => it).ToArray();
-
-                var tReturn = new List<T>();
-                object[] t = reader.NextRecord(tProps,tOrderedProps);
-                while (t != null)
+                object[] tAnon = reader.NextRecord(tProps, tOrderedProps);
+                while (tAnon != null)
                 {
 
-                    tReturn.Add((T)Activator.CreateInstance(tType, t));
-                    t = reader.NextRecord(tProps, tOrderedProps);
+                    tReturn.Add((T)Activator.CreateInstance(tType, tAnon));
+                    tAnon = reader.NextRecord(tProps, tOrderedProps);
                 }
 
 
 
                 return tReturn;
             }
-
-            return AllRecords<T>(reader);
-        }
-
-
-
-        /// <summary>
-        /// Return all the records T should be interface with getter properties that match types and names of the database.
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="reader">A reader.</param>
-        /// <returns></returns>
-        static public IEnumerable<T> AllRecords<T>(this DBFReader reader) where T:class 
-        {
-            var tType = typeof(T);
-
-                var tProperties = tType.GetProperties();
-                var tProps = tProperties
-                    .Select(
-                    it =>
-                    Array.FindIndex(reader.Fields,
-                                    jt => jt.Name.Equals(it.Name, StringComparison.InvariantCultureIgnoreCase))).ToArray();
-
-                var tOrderedProps = tProps.OrderBy(it => it).ToArray();
-                var tReturn = new List<T>();
-                object[] t = reader.NextRecord(tProps, tOrderedProps);  
+               
+            var t = reader.NextRecord(tProps, tOrderedProps);  
 
             while (t != null)
                 {
@@ -79,8 +68,15 @@ namespace DotNetDBF.Enumerable
             
         }
 
- 
 
+
+        /// <summary>
+        /// Returns a list of dynamic objects whose properties and types match up with that database name.
+        /// </summary>
+        /// <param name="reader">The reader.</param>
+        /// <param name="whereColumn">The where column name.</param>
+        /// <param name="whereColumnEquals">What the were column should equal.</param>
+        /// <returns></returns>
         static public IEnumerable<dynamic> DynamicAllRecords(this DBFReader reader, string whereColumn = null, dynamic whereColumnEquals = null)
         {
            
@@ -127,7 +123,7 @@ namespace DotNetDBF.Enumerable
             object[] GetDataRow();
         }
 
-        public class DBFIntercepter : ImpromptuObject
+        public class DBFIntercepter : ImpromptuObject, IDBFIntercepter
         {
             private readonly string[] _fieldNames;
             private readonly object[] _wrappedArray;
@@ -137,6 +133,11 @@ namespace DotNetDBF.Enumerable
                 _wrappedArray = wrappedObj;
                 _fieldNames = fieldNames;
 
+            }
+
+            public override IEnumerable<string> GetDynamicMemberNames()
+            {
+                return _fieldNames;
             }
 
             public override bool  TryGetMember(GetMemberBinder binder, out object result)
